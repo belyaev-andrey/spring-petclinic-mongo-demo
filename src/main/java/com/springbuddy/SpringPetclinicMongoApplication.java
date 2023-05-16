9b9a7958d526d95970771df2cd42948710ac8b42
belyaev-andrey <belyaev.andrey@gmail.com>
2023-05-16 22:06:19 +0200

    Initial commit

---
+++ src/main/java/com/springbuddy/SpringPetclinicMongoApplication.java
@@ -0,0 +1,13 @@
+package com.springbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPetclinicMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringPetclinicMongoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springbuddy/SpringPetclinicMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringPetclinicMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
