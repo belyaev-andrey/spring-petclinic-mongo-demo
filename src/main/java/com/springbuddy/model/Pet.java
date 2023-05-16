f48b6b5cfccae9249a4f9f886fca4203480af6d3
belyaev-andrey <belyaev.andrey@gmail.com>
2023-05-16 22:38:40 +0200

    Created branch for empty project

---
+++ src/main/java/com/springbuddy/model/Pet.java
@@ -0,0 +1,20 @@
+package com.springbuddy.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Getter
+@Setter
+@Document
+public class Pet {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    private Integer age;
+
+}
